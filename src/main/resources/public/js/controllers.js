285a57de5b43af56733d255d479d2c29cefc8803
Alexey <mr.gomel113@yandex.ru>
2018-12-15 02:36:09 +0300

    Update controllers.js

--- src/main/resources/public/js/controllers.js
+++ src/main/resources/public/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', []).controller('MaschineListController', function($scope, $state, popupService, $window, Maschine) {
-  $scope.maschines = Maschine.query(); //fetch all shipwrecks. Issues a GET to /api/vi/shipwrecks
-  $scope.deleteMaschine = function(maschine) { // Delete a Shipwreck. Issues a DELETE to /api/v1/shipwrecks/:id
+  $scope.maschines = Maschine.query(); 
+  $scope.deleteMaschine = function(maschine) { 
     if (popupService.showPopup('Really delete this?')) {
     	maschine.$delete(function() {
         $scope.maschines = Maschine.query(); 
@@ -9,25 +9,25 @@ angular.module('app.controllers', []).controller('MaschineListController', funct
     }
   };
 }).controller('MaschineViewController', function($scope, $stateParams, Maschine) {
-  $scope.maschine = Maschine.get({ id: $stateParams.id}); //Get a single shipwreck.Issues a GET to /api/v1/shipwrecks/:id
+  $scope.maschine = Maschine.get({ id: $stateParams.id}); 
 }).controller('MaschineCreateController', function($scope, $state, $stateParams, Maschine) {
-  $scope.maschine = new Maschine();  //create new shipwreck instance. Properties will be set via ng-model on UI
+  $scope.maschine = new Maschine();  
 
-  $scope.addMaschine = function() { //create a new shipwreck. Issues a POST to /api/v1/shipwrecks
+  $scope.addMaschine = function() { 
     $scope.maschine.$save(function() {
-      $state.go('maschines'); // on success go back to the list i.e. shipwrecks state.
+      $state.go('maschines'); 
     });
   };
 }).controller('MaschineEditController', function($scope, $state, $stateParams, Maschine) {
-  $scope.updateMaschine = function() { //Update the edited shipwreck. Issues a PUT to /api/v1/shipwrecks/:id
+  $scope.updateMaschine = function() { 
     $scope.maschine.$update(function() {
-      $state.go('maschines'); // on success go back to the list i.e. shipwrecks state.
+      $state.go('maschines');
     });
   };
 
-  $scope.loadMaschine = function() { //Issues a GET request to /api/v1/shipwrecks/:id to get a shipwreck to update
+  $scope.loadMaschine = function() { 
     $scope.maschine = Maschine.get({ id: $stateParams.id });
   };
 
-  $scope.loadMaschine(); // Load a shipwreck which can be edited on UI
+  $scope.loadMaschine(); 
 });
